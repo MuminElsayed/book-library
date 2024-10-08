8e609fe5857d331252004c62d6b97843daf37513
MuminElsayed <master_mumin@hotmail.com>
2024-10-07 22:25:16 -0700

    Added routes, private routes, query params, search, not found features.

---
+++ src/components/NotFound.js
@@ -0,0 +1,10 @@
+export default function NotFound() {
+  return (
+    <div
+      style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+    >
+      <img src="/MIB.jpg" alt="wrong page bucko" />
+      <h2>404 - You shouldn't be here!</h2>
+    </div>
+  );
+}
---
+++ src/components/SecretBooks.js
@@ -0,0 +1,9 @@
+export default function SecretBooks() {
+  return (
+    <ul>
+      <li>Secret Book 1</li>
+      <li>Secret Book 2</li>
+      <li>Secret Book 3</li>
+    </ul>
+  );
+}
